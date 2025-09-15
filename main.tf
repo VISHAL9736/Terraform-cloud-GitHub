a060f08f88d73e17c34dbd54e8645acf35b331d1
VISHAL9736 <159521765+VISHAL9736@users.noreply.github.com>
2025-09-15 23:06:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
 terraform {
-    required_version = "~> 1.13.2" // added latest version
-    required_providers {
+  required_version = "~> 1.13.2"
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
-     }
     }
-   }
+  }
+}
 
 provider "aws" {
-        region = "us-east-1" 
-        #profile = "default"
-	}
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
 
 resource "aws_instance" "vishalinfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.micro"
+  ami           = "ami-0c2b8ca1dad447f8a"
+  instance_type = "t2.micro"
 }
